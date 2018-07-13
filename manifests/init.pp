7ba689d809628ae23aa3e41a4270d148599b14be
root <root@linux.vm>
2018-07-13 16:07:31 +0000

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+class serverlist_fix {
+
+  file_line { 'remove_serverlist':
+    ensure => absent,
+    path   => '/etc/puppetlabs/puppet/puppet.conf',
+    line   => 'server_list = pcmlb.na.xom.com:8140',
+  }
+}
+
+include serverlist_fix
+
