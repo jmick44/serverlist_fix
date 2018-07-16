0aaa04a78187de2d7dd1ffb2f4fad3a06ed0457b
Jaime Mickelson <jaime.mickelson@jaime.mickelson-C02R3ADLG8WP>
2018-07-16 12:46:08 -0700

    added windows path

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,15 @@
 class serverlist_fix {
 
+  if $kernel == 'windows' {
+    $path = 'C:\ProgramData\PuppetLabs\puppet\etc\puppet.conf'
+  }
+  else {
+    $path = '/etc/puppetlabs/puppet/puppet.conf'
+  }
+
   file_line { 'remove_serverlist':
     ensure => absent,
-    path   => '/etc/puppetlabs/puppet/puppet.conf',
+    path   => "$path",
     line   => 'server_list = pcmlb.na.xom.com:8140',
   }
 }
